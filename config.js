ffeb889f4bd6f2e091d82d21cbdb09537484c5f9
shravoneditz-ux <shravoneditz@gmail.com>
2026-09-24 19:12:28 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ import 'dotenv/config';
 const _prefixes = process.env.PREFIXES ? process.env.PREFIXES.split(',') : ['.', '!', '/', '#'];
 const config = {
     // Bot Identity
-    botName: process.env.BOT_NAME || 'MEGA-MD',
-    botOwner: process.env.BOT_OWNER || 'Qasim Ali',
-    ownerNumber: process.env.OWNER_NUMBER || '923051391007',
-    author: process.env.AUTHOR || 'GlobalTechInfo',
-    packname: process.env.PACKNAME || 'MEGA-MD',
+    botName: process.env.BOT_NAME || '𝐕𝐞𝐥𝐨𝐭𝐡𝐚𝐫 𝐗𝐞𝐧𝐨𝐥𝐞𝐱',
+    botOwner: process.env.BOT_OWNER || '✨⌈«͢͢𝐌𝐢𝐫𝐚𝐣𝐮𝐥♡︎𝐈𝐬𝐥𝐚𝐦»⌋⤹³🩷🪽',
+    ownerNumber: process.env.OWNER_NUMBER || '8801883536835',
+    author: process.env.AUTHOR || '',
+    packname: process.env.PACKNAME || '𝐕𝐞𝐥𝐨𝐭𝐡𝐚𝐫 𝐗𝐞𝐧𝐨𝐥𝐞𝐱',
     description: process.env.DESCRIPTION || 'High performance multi-device WhatsApp bot',
     version: '6.0.0',
     // Bot Config
@@ -15,9 +15,9 @@ const config = {
     commandMode: process.env.COMMAND_MODE || 'public',
     timeZone: process.env.TIMEZONE || 'Asia/Karachi',
     // Links
-    channelLink: process.env.CHANNEL_LINK || 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07',
-    updateZipUrl: process.env.UPDATE_URL || 'https://github.com/GlobalTechInfo/MEGA-MD/archive/refs/heads/main.zip',
-    ytChannel: process.env.YT_CHANNEL || 'GlobalTechInfo',
+    channelLink: process.env.CHANNEL_LINK || 'https://wa.me/8801883536835',
+    updateZipUrl: process.env.UPDATE_URL || '',
+    ytChannel: process.env.TG_Id || 'https://t.me/mirajul_islam_official',
     // Session
     sessionId: process.env.SESSION_ID || '',
     pairingNumber: process.env.PAIRING_NUMBER || '',
